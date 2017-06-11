74bba734fb052a4345c893373782606bd7a5ca0b
pjlangley <langleyjpeter@gmail.com>
2017-06-11 15:44:53 +0100

    New homepage HTML.

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Code Chewing</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,700" rel="stylesheet">
+    <link href="/public/assets/css/styles.css" rel="stylesheet">
+</head>
+<body>
+<!--[if lte IE 9]>
+    <p class="upgrade-browser">
+    You are using an outdated browser. Please
+    <a href="https://browsehappy.com" rel="nofollow noopener noreferrer" target="_blank">upgrade your browser</a>
+    to improve your experience and security.
+    </p>
+<![endif]-->
+
+<div class="banner">
+    <div class="container">
+        <header class="row header">
+            <div class="col-sm-12 text-center">
+                <img src="/public/assets/img/code-chewing-logo.png" alt="Code Chewing Logo" class="logo">
+                <h1>We build software - websites, mobile &amp; desktop apps.</h1>
+            </div>
+        </header>
+    </div>
+</div>
+
+<div class="content">
+    <div class="container">
+        <div class="row contact">
+            <div class="col-sm-12">
+                <img src="/public/assets/img/mobile-phone.png" alt="Mobile Phone" class="mobile-phone">
+            </div>
+            <div class="col-sm-12">
+                <p>
+                    Get in touch with us via
+                    <a href="https://twitter.com/codechewing"
+                       rel="nofollow noopener noreferrer"
+                       target="_blank"
+                       title="Code Chewing - twitter">@codechewing</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row other-things">
+            <div class="col-sm-6">
+                <small class="title">Other things from us</small>
+                <h2>
+                    <a href="https://guides.codechewing.com"
+                       rel="nofollow noopener noreferrer"
+                       target="_blank"
+                       title="Code Chewing Guides"
+                    >
+                        Code Chewing Guides
+                    </a>
+                </h2>
+                <p>
+                    Code Chewing Guides target challenges within web technology.
+                    Providing free, educational solutions, written in a straight to the point manner.
+                    <a href="https://guides.codechewing.com"
+                       rel="nofollow noopener noreferrer"
+                       target="_blank"
+                       title="Code Chewing Guides"
+                    >
+                        Check it out.
+                    </a>
+                </p>
+
+                <p>
+                    For our older blog -
+                    <a href="http://www.codechewing.com/library" rel="nofollow noopener noreferrer" target="_blank">
+                       see here.
+                    </a>
+                </p>
+            </div>
+        </div>
+        <div class="row company-info">
+            <div class="col-sm-6">
+                <ul>
+                    <li>Code Chewing Ltd</li>
+                    <li>Company no. 10140190</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="public/assets/js/ga.js"></script>
+</body>
+</html>
